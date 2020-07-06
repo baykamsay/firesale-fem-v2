f67fb49a66c3e8bff928189b7aee83ff9c34a957
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-07-06 14:20:14 +0300

    Update renderer.js

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const marked = require('marked');
 const { remote, ipcRenderer } = require('electron');
 const mainProcess = remote.require('./main');
+const currentWindow = remote.getCurrentWindow();
+
+let filePath = null;
+let originalContent = '';
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -16,6 +21,16 @@ const renderMarkdownToHtml = markdown => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateUserInterface = () => {
+  let title = 'Fire Sale';
+
+  if (filePath) {
+    title = `${path.basename(filePath)} - ${title}`;
+  }
+
+  currentWindow.setTitle(title);
+};
+
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
@@ -26,6 +41,11 @@ openFileButton.addEventListener('click', () => {
 });
 
 ipcRenderer.on('file-opened', (event, file, content) => {
+  filePath = file;
+  originalContent = content;
+
   markdownView.value = content;
   renderMarkdownToHtml(content);
+
+  updateUserInterface();
 });
